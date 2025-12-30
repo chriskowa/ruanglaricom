3ff99366e9a7faef3d36566edfadef4aee0595e9
chriskowa <christianus.lusi@gmail.com>
2025-12-30 08:32:21 +0700

    update

--- app/Http/Controllers/RunnerProfileController.php
+++ app/Http/Controllers/RunnerProfileController.php
@@ -9,7 +9,17 @@ class RunnerProfileController extends Controller
 {
     public function show($username)
     {
-        $user = User::where('username', $username)->firstOrFail();
+        // Check if input is numeric (ID) or string (username)
+        if (is_numeric($username)) {
+            $user = User::where('id', $username)->first();
+            
+            // If not found by ID, try username just in case user has numeric username (unlikely but possible)
+            if (!$user) {
+                $user = User::where('username', $username)->firstOrFail();
+            }
+        } else {
+            $user = User::where('username', $username)->firstOrFail();
+        }
         
         // Ensure user is a runner or coach (coaches are also runners usually)
         // But maybe strictly 'runner'? The prompt says "profile detail user pada public ... lebih runner".
