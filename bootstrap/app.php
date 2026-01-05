c2b5fb2d4c69d232fcd016bac019d263b68f9cbd
chriskowa <christianus.lusi@gmail.com>
2026-01-05 20:55:04 +0700

    bug fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->validateCsrfToken(except: [
+        $middleware->validateCsrfTokens(except: [
             'events/transactions/webhook',
             'marketplace/webhook',
             'wallet/topup/callback',
