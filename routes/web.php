9ab40794f7f288561c5b6e32a40bb5e2abbac1bd
chriskowa <christianus.lusi@gmail.com>
2026-01-21 20:42:11 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -143,7 +143,7 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->grou
 });
 
 // Public routes
-Route::get('/calculator', [App\Http\Controllers\CalculatorController::class, 'index'])->name('calculator');
+Route::get('/tools/calculator', [App\Http\Controllers\CalculatorController::class, 'index'])->name('calculator');
 Route::get('/tools/form-analyzer', [App\Http\Controllers\FormAnalyzerController::class, 'index'])->name('tools.form-analyzer');
 Route::post('/tools/form-analyzer/analyze', [App\Http\Controllers\FormAnalyzerController::class, 'analyze'])->name('tools.form-analyzer.analyze');
 Route::get('/calendar', [App\Http\Controllers\CalendarController::class, 'index'])->name('calendar.public');
