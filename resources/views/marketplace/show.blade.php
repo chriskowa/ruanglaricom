6d72ee6cc8a3532e8d9d6f62deaade9fb22a9a1d
chriskowa <christianus.lusi@gmail.com>
2026-01-24 17:04:51 +0700

    fiz

--- resources/views/marketplace/show.blade.php
+++ resources/views/marketplace/show.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.pacerhub')
 @php($withSidebar = true)
 
-@section('title', $product->title . ' - Marketplace')
+@section('title')
+    {{ $product->title }} - Marketplace
+@endsection
 
 @section('content')
 <div class="min-h-screen pt-20 pb-10 px-4 md:px-8 relative overflow-hidden font-sans">
