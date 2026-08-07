4e88530ab5dca99726fc19b630a1d292835b16dc
chriskowa <christianus.lusi@gmail.com>
2026-08-07 16:30:45 +0700

    bug fix

--- resources/views/events/running-event-detail.blade.php
+++ resources/views/events/running-event-detail.blade.php
@@ -120,7 +120,7 @@
                                     <div class="absolute top-0 right-0 p-2 opacity-10 group-hover:opacity-20 transition-opacity">
                                         <svg class="w-12 h-12 text-neon" fill="currentColor" viewBox="0 0 24 24"><path d="M13.5 2c-5.621 0-10.211 4.443-10.475 10h-3.025l5 6.625 5-6.625h-2.975c.257-3.351 3.06-6 6.475-6 3.584 0 6.5 2.916 6.5 6.5s-2.916 6.5-6.5 6.5c-1.863 0-3.542-.793-4.728-2.053l-2.427 3.216c1.877 1.754 4.389 2.837 7.155 2.837 5.79 0 10.5-4.71 10.5-10.5s-4.71-10.5-10.5-10.5z"/></svg>
                                     </div>
-                                    <span class="relative z-10 block text-3xl font-black text-white group-hover:text-neon mb-1 transition-colors">{{ $distance->name }}</span>
+                                    <span class="relative z-10 block text-2xl font-black text-white group-hover:text-neon mb-1 transition-colors">{{ $distance->name }}</span>
                                     <span class="relative z-10 text-xs text-slate-500 uppercase tracking-wider font-bold">Distance</span>
                                 </div>
                             @endforeach
