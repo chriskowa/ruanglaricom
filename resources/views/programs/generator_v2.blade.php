e49c4a934deb6c440964b735e3be54550385ed38
chriskowa <christianus.lusi@gmail.com>
2026-09-14 09:58:11 +0700

    bug fix

--- resources/views/programs/generator_v2.blade.php
+++ resources/views/programs/generator_v2.blade.php
@@ -417,10 +417,10 @@
 
                 <!-- Feature Highlights -->
                 <div class="grid grid-cols-3 gap-3 pt-1">
-                    <div class="p-3 rounded-md bg-[#131B2D] border border-slate-800">
+                    <!--<div class="p-3 rounded-md bg-[#131B2D] border border-slate-800">
                         <div class="text-[10px] text-slate-400 uppercase font-medium">Metode Ilmiah</div>
                         <div class="text-xs font-bold text-white mt-0.5">Jack Daniels VDOT</div>
-                    </div>
+                    </div>-->
                     
                 </div>
 
